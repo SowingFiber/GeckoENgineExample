a92903ea38acd7b216242bafa06db0072d9aabad
Shanu <iamshanuuknow@outlook.com>
2019-08-24 19:08:21 +0530

    Upload

---
+++ app/src/main/java/com/geckoengine/example/MainActivity.java
@@ -0,0 +1,29 @@
+package com.geckoengine.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import org.mozilla.geckoview.GeckoRuntime;
+import org.mozilla.geckoview.GeckoSession;
+import org.mozilla.geckoview.GeckoView;
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        GeckoView view = findViewById(R.id.geckoview);
+        GeckoSession session = new GeckoSession();
+        GeckoRuntime runtime = GeckoRuntime.create(this);
+
+        session.open(runtime);
+        view.setSession(session);
+
+
+        //TODO: add any url
+        //TODO: Use intent.putExtraString() to send a url from the main activity to this activity
+        session.loadUri("http://theyouthbuzz.com/");
+    }
+}
